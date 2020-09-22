63a514c4371a99b580c3e738fd90443c63c423af
tinagerosrozas <you@example.com>
2020-09-22 17:36:09 -0500

    agregar

--- lab2c/main/urls.py
+++ lab2c/main/urls.py
@@ -3,9 +3,15 @@ from . import  views
 
 urlpatterns = [
 
-    path ('', views.home, name='home'),
-    path('productos', views.ProductListView.as_view(), name='product-list'),
-    path('productos/<int:pk>', views.ProductDetailView.as_view(), name='product-detail'),
+    path('', views.home, name='home'),
+    path('productos', views.ProductListView.as_view(), name='product_list'),
+    path('productos/<int:pk>', views.ProductDetailView.as_view(), name='product_detail'),
+    path('proveedores', views.ProveedorListView.as_view(), name='proveedor_list'),
+    path('proveedores/<int:pk>', views.ProveedorDetailView.as_view(), name='proveedor_detail'),
+    path('categoria', views.CategoriaListView.as_view(), name='categoria_list'),
+    path('categoria/<int:pk>', views.CategoriaDetailView.as_view(), name='categoria_detail'),
+    path('localizacion', views.LocalizacionListView.as_view(), name='localizacion_list'),
+    path('localizacion/<int:pk>', views.LocalizacionDetailView.as_view(), name='localizacion_detail'),
 
 
 ]
--- lab2c/main/views.py
+++ lab2c/main/views.py
@@ -1,11 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import ListView, DetailView
-from . models import Producto
+from . models import *
 # Create your views here.
 def home (request):
-    return HttpResponse ("Hola Mundo de nuevo")
+    return HttpResponse ("Hola Mundo. Te encuentras en la página de inicio de Linio Express")
 class ProductListView(ListView):
     model = Producto
 class ProductDetailView(DetailView):
     model = Producto
+
+class ProveedorListView(ListView):
+  model = Proveedor
+
+class ProveedorDetailView(DetailView):
+  model = Proveedor
+
+class CategoriaListView(ListView):
+  model = Categoria
+
+class CategoriaDetailView(DetailView):
+  model = Categoria
+
+class LocalizacionListView(ListView):
+  model = Localizacion
+
+class LocalizacionDetailView(DetailView):
+  model = Localizacion
